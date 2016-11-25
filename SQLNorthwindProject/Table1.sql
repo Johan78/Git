ab7429d48bdadc3e69ff1ba3bfcc0bf298d6f43d
Johan Rignell <rignell78@hotmail.com>
2016-11-25 14:22:47 +0100

    customername nytt table

---
+++ SQLNorthwindProject/Table1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[CustomerNameUpdate](
+    [CustomerNameUpdateID] [int] IDENTITY(1,1) NOT NULL,
+    [CustomerID] [nchar](5) NOT NULL,
+    [OldName] [varchar](30) NOT NULL,
+    [NewName] [varchar](30) NOT NULL,
+    [ChangedDate] [datetime] NOT NULL,
+    PRIMARY KEY CLUSTERED 
+    (
+        [CustomerNameUpdateID] ASC
+    )
+)
+
+GO
